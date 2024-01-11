5c319735255a22821734d99e4d150da81f21820a
Sergio del Amo <sergio.delamo@softamo.com>
2024-01-11 16:10:27 +0100

    handler-java-serde

---
+++ handler-java-serde/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.micronaut.minimal.application") version "4.1.2"
+}
+
+version = "0.1"
+group = "com.example"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("io.micronaut.aws:micronaut-aws-lambda-events-serde")
+
+    implementation("io.micronaut.aws:micronaut-aws-apigateway")
+    implementation("io.micronaut.aws:micronaut-function-aws")
+    runtimeOnly("ch.qos.logback:logback-classic")
+}
+application {
+    mainClass.set("com.example.Application")
+}
+
+java {
+    sourceCompatibility = JavaVersion.toVersion("17")
+    targetCompatibility = JavaVersion.toVersion("17")
+}
+
+micronaut {
+    runtime("lambda_java")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("com.example.*")
+    }
+}
+
+
+
